56cfd961e7a48e1c6d917fb25d0464955ffedc1e
XXC的天选3\89404 <894043590@qq.com>
2023-05-15 07:34:48 +0800

    27_区域v2的封装和初次渲染的数据

--- src/views/home/index.jsx
+++ src/views/home/index.jsx
@@ -1,13 +1,12 @@
-import React, { memo, useCallback, useEffect, useState } from "react";
+import React, { memo, useEffect } from "react";
 import { shallowEqual, useDispatch, useSelector } from "react-redux";
 
 import { HomeWrapper } from "./style";
 import { fetchHomeDataAction } from "@/store/modules/home";
 import HomeBanner from "./c-cpns/home-banner";
 import HomeSectionV1 from "./c-cpns/home-section-v1";
-import SectionHeader from "@/components/section-header";
-import SectionRooms from "@/components/section-rooms";
-import SectionTabs from "@/components/section-tabs";
+import HomeSectionV2 from "./c-cpns/home-section-v2";
+import { isEmptyObject } from "@/utils";
 
 const Home = memo(() => {
   /* 从redux中获取数据 */
@@ -20,14 +19,6 @@ const Home = memo(() => {
     shallowEqual
   );
 
-  /* 数据的转换 */
-  const [name, setName] = useState("佛山");
-  const tabNames = discountInfo.dest_address?.map((item) => item.name);
-
-  const tabClickHandle = useCallback(function (index, name) {
-    setName(name);
-  }, []);
-
   // 发起进行的网络请求
   const dispatch = useDispatch();
   useEffect(() => {
@@ -39,20 +30,16 @@ const Home = memo(() => {
       <HomeBanner />
       <div className="content">
         {/* 折扣数据 */}
-        <div className="discount">
-          <SectionHeader
-            title={discountInfo.title}
-            subtitle={discountInfo.subtitle}
-          />
-          <SectionTabs tabNames={tabNames} tabClick={tabClickHandle} />
-          <SectionRooms
-            roomList={discountInfo.dest_list?.[name]}
-            itemWidth="33.33%"
-          />
-        </div>
+        {isEmptyObject(discountInfo) && (
+          <HomeSectionV2 infoData={discountInfo} />
+        )}
 
-        <HomeSectionV1 infoData={goodPriceInfo} />
-        <HomeSectionV1 infoData={highScoreInfo} />
+        {isEmptyObject(goodPriceInfo) && (
+          <HomeSectionV1 infoData={goodPriceInfo} />
+        )}
+        {isEmptyObject(highScoreInfo) && (
+          <HomeSectionV1 infoData={highScoreInfo} />
+        )}
       </div>
     </HomeWrapper>
   );
