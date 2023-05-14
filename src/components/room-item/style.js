6626d0bd34f946f4ccbeb9effd3cb1050a47f38a
XXC的天选3\89404 <894043590@qq.com>
2023-05-14 16:14:34 +0800

    18_房间Item的布局的展示过程

--- src/components/room-item/style.js
+++ src/components/room-item/style.js
@@ -4,4 +4,63 @@ export const ItemWrapper = styled.div`
   box-sizing: border-box;
   width: 25%;
   padding: 8px;
+
+  .inner {
+    width: 100%;
+  }
+
+  .cover {
+    position: relative;
+    box-sizing: border-box;
+    /* padding-top使用百分比相对的是宽度 */
+    padding: 66.66% 8px 0;
+    border-radius: 3px;
+    overflow: hidden;
+    /* 之后再让 img 标签使用绝对定位，保证其不会占据上方Padding的位置，然后让其Height为 100%，让其占满cover */
+    img {
+      position: absolute;
+      left: 0;
+      top: 0;
+      width: 100%;
+      height: 100%;
+    }
+  }
+
+  .desc {
+    margin: 10px 0 5px;
+    font-size: 12px;
+    font-weight: 700;
+    color: ${(props) => props.verifyColor};
+  }
+
+  .name {
+    font-size: 16px;
+    font-weight: 700;
+
+    overflow: hidden;
+    text-overflow: ellipsis;
+    display: -webkit-box;
+    -webkit-line-clamp: 2;
+    -webkit-box-orient: vertical;
+  }
+
+  .price {
+    margin: 8px 0;
+  }
+
+  .bottom {
+    display: flex;
+    align-items: center;
+    font-size: 12px;
+    font-weight: 600;
+    color: ${(props) => props.theme.text.primaryColor};
+
+    .count {
+      margin: 0 2px 0 4px;
+    }
+
+    .MuiRating-decimal {
+      margin-right: -2px;
+    }
+  }
 `;
