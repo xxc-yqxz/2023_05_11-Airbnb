dd168668023a4cd83b3d7c34d458e2053658f06c
XXC的天选3\89404 <894043590@qq.com>
2023-05-15 21:25:00 +0800

    31_ScrollView右边按钮点击

--- src/base-ui/scroll-view/index.jsx
+++ src/base-ui/scroll-view/index.jsx
@@ -5,6 +5,8 @@ import { ViewWrapper } from "./style";
 const ScrollView = memo((props) => {
   /* 定义内部的状态 */
   const [showRight, setShowRight] = useState(false);
+  const [posIndex, setPosIndex] = useState(0);
+  const totalDistanceRef = useRef(); // 此处不用useState是因为其每次调用对应的set方法，都会导致组件重新刷新
 
   /* 组件渲染完毕，判断是否显示右侧的按钮 */
   const scrollContentRef = useRef();
@@ -12,13 +14,28 @@ const ScrollView = memo((props) => {
     const scrollWidth = scrollContentRef.current.scrollWidth; // 一共可以滚动的宽度
     const clientWidth = scrollContentRef.current.clientWidth; // 本身占据的宽度
     const totalDistance = scrollWidth - clientWidth;
+    totalDistanceRef.current = totalDistance;
     setShowRight(totalDistance > 0);
   }, [props.children]);
+
+  /* 事件处理的逻辑 */
+  function rightClickHandle() {
+    const newIndex = posIndex + 1;
+    const newEl = scrollContentRef.current.children[newIndex];
+    // offsetLeft相对的时其有带定位的父元素
+    // console.log(newEl.offsetLeft);
+
+    const newOffsetLeft = newEl.offsetLeft;
+    scrollContentRef.current.style.transform = `translate(-${newOffsetLeft}px)`;
+    setPosIndex(newIndex);
+    // 是否继续显示右侧的按钮
+    setShowRight(totalDistanceRef.current > newOffsetLeft);
+  }
 
   return (
     <ViewWrapper>
       <button>左边按钮</button>
-      {showRight && <button>右边按钮</button>}
+      {showRight && <button onClick={rightClickHandle}>右边按钮</button>}
 
       <div className="scroll-content" ref={scrollContentRef}>
         {props.children}
--- src/base-ui/scroll-view/style.js
+++ src/base-ui/scroll-view/style.js
@@ -3,6 +3,9 @@ import styled from "styled-components";
 export const ViewWrapper = styled.div`
   overflow: hidden;
   .scroll-content {
+    position: relative;
     display: flex;
+
+    transition: transform 250ms ease;
   }
 `;
