54add6c28e928b263bd5a36df38fd201d670dfcf
XXC的天选3\89404 <894043590@qq.com>
2023-05-16 23:23:38 +0800

    39_页码的组件展示和展示数据算法

--- src/views/entire/c-cpns/entire-pagination/style.js
+++ src/views/entire/c-cpns/entire-pagination/style.js
@@ -1,3 +1,33 @@
 import styled from "styled-components";
 
-export const PaginationWrapper = styled.div``;
+export const PaginationWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+
+  .info {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    .MuiPaginationItem-page {
+      margin: 0 9px;
+
+      &:hover {
+        text-decoration: underline;
+      }
+    }
+    .MuiPaginationItem-icon {
+      font-size: 24px;
+    }
+
+    .MuiPaginationItem-page.Mui-selected {
+      background-color: #222;
+      color: #fff;
+    }
+
+    .desc {
+      margin-top: 16px;
+      color: #222;
+    }
+  }
+`;
