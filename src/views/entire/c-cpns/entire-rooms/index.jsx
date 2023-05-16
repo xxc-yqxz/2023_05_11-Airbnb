e284b27462d45ff5a745e2073ec96491a251e451
XXC的天选3\89404 <894043590@qq.com>
2023-05-16 22:43:23 +0800

    38_房间列表数据的展示过程

--- src/views/entire/c-cpns/entire-rooms/index.jsx
+++ src/views/entire/c-cpns/entire-rooms/index.jsx
@@ -1,8 +1,25 @@
 import React, { memo } from "react";
 import { RoomsWrapper } from "./style";
+import { useSelector } from "react-redux";
+import RoomItem from "@/components/room-item";
 
 const EntireRooms = memo(() => {
-  return <RoomsWrapper>EntireRooms</RoomsWrapper>;
+  /* 从redux中获取roomList数据 */
+  const { roomList, totalCount } = useSelector((state) => ({
+    roomList: state.entire.roomList,
+    totalCount: state.entire.totalCount,
+  }));
+
+  return (
+    <RoomsWrapper>
+      <h2 className="title">{totalCount}多处住所</h2>
+      <div className="list">
+        {roomList.map((item) => {
+          return <RoomItem itemData={item} itemWidth="20%" key={item.id} />;
+        })}
+      </div>
+    </RoomsWrapper>
+  );
 });
 
 export default EntireRooms;
--- src/views/entire/c-cpns/entire-rooms/style.js
+++ src/views/entire/c-cpns/entire-rooms/style.js
@@ -1,3 +1,15 @@
 import styled from "styled-components";
 
-export const RoomsWrapper = styled.div``;
+export const RoomsWrapper = styled.div`
+  padding: 40px 20px;
+  .title {
+    font-size: 22px;
+    font-weight: 700;
+    color: #222;
+    margin: 0 0 10px 10px;
+  }
+  .list {
+    display: flex;
+    flex-wrap: wrap;
+  }
+`;
