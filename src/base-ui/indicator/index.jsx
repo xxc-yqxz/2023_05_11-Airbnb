1d1c8c89ea7ebff54a3f496d7e9ab5c2f55a186c
XXC的天选3\89404 <894043590@qq.com>
2023-05-17 21:53:45 +0800

    44_Indicator组件封装-滚动位置的实现

--- src/base-ui/indicator/index.jsx
+++ src/base-ui/indicator/index.jsx
@@ -1,15 +1,38 @@
 import PropTypes from "prop-types";
-import React, { memo } from "react";
+import React, { memo, useEffect, useRef } from "react";
 import { IndicatorWrapper } from "./style";
 
 const Indicator = memo((props) => {
+  const { selectIndex } = props;
+  const contentRef = useRef();
+
+  useEffect(() => {
+    // 1.获取selectIndex对应的item
+    const selectItemEl = contentRef.current.children[selectIndex];
+    const itemLeft = selectItemEl.offsetLeft;
+    const itemWidth = selectItemEl.clientWidth;
+
+    // 2.content的宽度
+    const contentWidth = contentRef.current.clientWidth;
+    console.log(contentWidth);
+
+    // 3.获取selectIndex要滚动的距离
+    const distance = itemLeft + itemWidth * 0.5 - contentWidth * 0.5;
+
+    contentRef.current.style.transform = `translate(${-distance}px)`;
+  }, [selectIndex]);
+
   return (
     <IndicatorWrapper>
-      <div className="i-content">{props.children}</div>
+      <div className="i-content" ref={contentRef}>
+        {props.children}
+      </div>
     </IndicatorWrapper>
   );
 });
 
-Indicator.propTypes = {};
+Indicator.propTypes = {
+  selectIndex: PropTypes.number,
+};
 
 export default Indicator;
