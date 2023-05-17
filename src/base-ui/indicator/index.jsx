5fd8d0d02e19c86a9d552b8778077e1897819687
XXC的天选3\89404 <894043590@qq.com>
2023-05-17 22:09:53 +0800

    45_Indicator组件封装-左右特殊情况

--- src/base-ui/indicator/index.jsx
+++ src/base-ui/indicator/index.jsx
@@ -14,11 +14,17 @@ const Indicator = memo((props) => {
 
     // 2.content的宽度
     const contentWidth = contentRef.current.clientWidth;
+    const contentScroll = contentRef.current.scrollWidth;
     console.log(contentWidth);
 
     // 3.获取selectIndex要滚动的距离
-    const distance = itemLeft + itemWidth * 0.5 - contentWidth * 0.5;
+    let distance = itemLeft + itemWidth * 0.5 - contentWidth * 0.5;
+    // 4.特殊情况的处理
+    if (distance < 0) distance = 0; // 左边的特殊情况
+    const totalDistance = contentScroll - contentWidth;
+    if (distance > totalDistance) distance = totalDistance; //  右边的特殊情况处理
 
+    // 5.改变位置
     contentRef.current.style.transform = `translate(${-distance}px)`;
   }, [selectIndex]);
 
