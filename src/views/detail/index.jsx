e72851de5b6aaf78b03271e3d8183967b36dbbc1
XXC的天选3\89404 <894043590@qq.com>
2023-05-18 07:36:00 +0800

    49_详情的图片展示和遮盖层效果

--- src/views/detail/index.jsx
+++ src/views/detail/index.jsx
@@ -1,12 +1,16 @@
 import React, { memo } from "react";
 import { useSelector } from "react-redux";
+import { DetailWrapper } from "./style";
+import DetailPictures from "./c-cpns/detail-pictures";
+import DetailInfos from "./c-cpns/detail-infos";
 
 const Detail = memo(() => {
-  const { detailInfo } = useSelector((state) => ({
-    detailInfo: state.detail.detailInfo,
-  }));
-
-  return <div>{detailInfo.name}</div>;
+  return (
+    <DetailWrapper>
+      <DetailPictures />
+      <DetailInfos />
+    </DetailWrapper>
+  );
 });
 
 export default Detail;
