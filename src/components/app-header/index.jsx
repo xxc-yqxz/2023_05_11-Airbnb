4e2bf31415fbb03db38672fd99723b727bf80012
XXC的天选3\89404 <894043590@qq.com>
2023-05-19 19:50:56 +0800

    58_监听滚动效果的消失

--- src/components/app-header/index.jsx
+++ src/components/app-header/index.jsx
@@ -1,10 +1,11 @@
-import React, { memo, useState } from "react";
+import React, { memo, useRef, useState } from "react";
 import { HeaderWrapper, SearchAreaWrapper } from "./style";
 import HeaderLeft from "./c-cpns/header-left";
 import HeaderCenter from "./c-cpns/header-center";
 import HeaderRight from "./c-cpns/header-right";
 import { shallowEqual, useSelector } from "react-redux";
 import classNames from "classnames";
+import useScrollPosition from "@/hooks/useScrollPosition";
 
 const AppHeader = memo(() => {
   /* 定义组件内部的状态 */
@@ -19,7 +20,19 @@ const AppHeader = memo(() => {
   );
 
   const { isFixed } = headerConfig;
-  console.log(isFixed);
+
+  /* 监听滚动 */
+  // 使用了useScrollPosition后，每次改变内部的useState的值后，都会重新渲染AppHeader组件
+  const { scrollY } = useScrollPosition();
+  const prevY = useRef(0);
+  // 在正常情况下（搜索框没有弹出），不断记录值
+  if (!isSearch) {
+    prevY.current = scrollY;
+  }
+  // 在弹出搜索功能的情况下，滚动的距离大于之前记录的距离的30
+  if (isSearch && Math.abs(scrollY - prevY.current) > 30) {
+    setIsSearch(false);
+  }
 
   return (
     <HeaderWrapper className={classNames({ fixed: isFixed })}>
